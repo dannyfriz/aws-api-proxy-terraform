d1ef2e04715eb22a4641fa808c781ab966d5d13f
dcfriz <dny.friz@gmail.com>
2023-06-19 02:07:40 -0400

    orden

--- main.tf
+++ main.tf
@@ -8,79 +8,62 @@ terraform {
   }
 
   required_version = ">= 1.4.5"
-
 }
 
+# Proveedor de AWS
 provider "aws" {
   region = var.aws_region
 }
 
-# Carga los valores de las variables del archivo .tfvars de uso global
-variable "aws_region" {}
-variable "api_name" {}
+# Variables
 variable "api_description" {}
+variable "api_name" {}
 variable "api_stage_name" {}
-variable "dynamodb_table_name" {}
+variable "aws_account_id" {}
+variable "aws_region" {}
 variable "dynamodb_billing_mode" {}
 variable "dynamodb_read_capacity" {}
+variable "dynamodb_table_name" {}
 variable "dynamodb_write_capacity" {}
-variable "lambda_function_name" {}
-variable "lambda_runtime" {}
-variable "lambda_handler" {}
-variable "lambda_timeout" {}
-variable "lambda_memory_size" {}
 variable "lambda_function_code_path" {}
-variable "aws_account_id" {}
+variable "lambda_function_name" {}
+variable "lambda_handler" {}
+variable "lambda_memory_size" {}
+variable "lambda_runtime" {}
+variable "lambda_timeout" {}
 variable "notification_email" {}
 
-# Carga los valores de las variables del archivo .tfvars como variables locales
-locals {
-  aws_region                = var.aws_region
-  aws_account_id                = var.aws_account_id
-  api_name                  = var.api_name
-  api_description           = var.api_description
-  api_stage_name            = var.api_stage_name
-  dynamodb_table_name       = var.dynamodb_table_name
-  dynamodb_billing_mode     = var.dynamodb_billing_mode
-  dynamodb_read_capacity    = var.dynamodb_read_capacity
-  dynamodb_write_capacity   = var.dynamodb_write_capacity
-  lambda_function_name      = var.lambda_function_name
-  lambda_runtime            = var.lambda_runtime
-  lambda_handler            = var.lambda_handler
-  lambda_timeout            = var.lambda_timeout
-  lambda_memory_size        = var.lambda_memory_size
-  lambda_function_code_path = var.lambda_function_code_path
-  notification_email = var.notification_email
-}
-
+# Módulo: api_gateway
 module "api_gateway" {
-  source = "./modules/api_gateway"
-  api_name         = var.api_name
-  api_description  = var.api_description
-  api_stage_name   = var.api_stage_name
-  aws_region           = var.aws_region
-  lambda_function_arn = module.lambda_function.lambda_function_arn
-  lambda_function_name = module.lambda_function.lambda_function_name
-  notification_email = var.notification_email
-  aws_account_id = var.aws_account_id
+  source                 = "./modules/api_gateway"
+  api_description        = var.api_description
+  api_name               = var.api_name
+  api_stage_name         = var.api_stage_name
+  aws_account_id         = var.aws_account_id
+  aws_region             = var.aws_region
+  lambda_function_arn    = module.lambda_function.lambda_function_arn
+  lambda_function_name   = module.lambda_function.lambda_function_name
+  notification_email     = var.notification_email
 }
 
+# Módulo: lambda_function
 module "lambda_function" {
-  source             = "./modules/lambda_function"
-  lambda_function_name      = var.lambda_function_name
-  lambda_runtime            = var.lambda_runtime
-  lambda_handler            = var.lambda_handler
-  lambda_timeout            = var.lambda_timeout
-  lambda_memory_size        = var.lambda_memory_size
-  lambda_function_code_path = var.lambda_function_code_path
+  source                     = "./modules/lambda_function"
   api_gateway_deployment_arn = module.api_gateway.api_gateway_deployment_arn
   aws_region                 = var.aws_region
+  lambda_function_code_path  = var.lambda_function_code_path
+  lambda_function_name       = var.lambda_function_name
+  lambda_handler             = var.lambda_handler
+  lambda_memory_size         = var.lambda_memory_size
+  lambda_runtime             = var.lambda_runtime
+  lambda_timeout             = var.lambda_timeout
 }
 
+# Módulo: dynamodb
 module "dynamodb" {
-  source = "./modules/dynamodb"
-  dynamodb_table_name      = var.dynamodb_table_name
+  source                   = "./modules/dynamodb"
   dynamodb_billing_mode    = var.dynamodb_billing_mode
   dynamodb_read_capacity   = var.dynamodb_read_capacity
+  dynamodb_table_name      = var.dynamodb_table_name
   dynamodb_write_capacity  = var.dynamodb_write_capacity
 }
--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -1,20 +1,24 @@
+# Recurso para la API REST de API Gateway
 resource "aws_api_gateway_rest_api" "api" {
   name        = var.api_name
   description = var.api_description
 }
 
+# Recurso para el recurso "categories" de la API Gateway
 resource "aws_api_gateway_resource" "categories_resource" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
   path_part   = "categories"
 }
 
+# Recurso para el recurso "categories_proxy" de la API Gateway
 resource "aws_api_gateway_resource" "categories_proxy_resource" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_resource.categories_resource.id
   path_part   = "{proxy+}"
 }
 
+# Método GET para el recurso "categories_proxy" de la API Gateway
 resource "aws_api_gateway_method" "get_categories_proxy_method" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.categories_proxy_resource.id
@@ -26,6 +30,7 @@ resource "aws_api_gateway_method" "get_categories_proxy_method" {
   }
 }
 
+# Integración HTTP_PROXY para el método GET de "categories_proxy" de la API Gateway
 resource "aws_api_gateway_integration" "get_categories_proxy_integration" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.categories_proxy_resource.id
@@ -41,6 +46,7 @@ resource "aws_api_gateway_integration" "get_categories_proxy_integration" {
   passthrough_behavior = "WHEN_NO_MATCH"
 }
 
+# Respuesta de integración exitosa para el método GET de "categories_proxy" de la API Gateway
 resource "aws_api_gateway_integration_response" "ok_integration_response" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.categories_proxy_resource.id
@@ -52,6 +58,7 @@ resource "aws_api_gateway_integration_response" "ok_integration_response" {
   }
 }
 
+# Respuesta exitosa para el método GET de "categories_proxy" de la API Gateway
 resource "aws_api_gateway_method_response" "ok_response" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.categories_proxy_resource.id
@@ -63,6 +70,7 @@ resource "aws_api_gateway_method_response" "ok_response" {
   }
 }
 
+# Respuesta de error para el método GET de "categories_proxy" de la API Gateway
 resource "aws_api_gateway_method_response" "bad_request_response" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.categories_proxy_resource.id
@@ -74,6 +82,7 @@ resource "aws_api_gateway_method_response" "bad_request_response" {
   }
 }
 
+# Método OPTIONS para el recurso "categories_proxy" de la API Gateway
 resource "aws_api_gateway_method" "categories_options" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.categories_proxy_resource.id
@@ -81,6 +90,7 @@ resource "aws_api_gateway_method" "categories_options" {
   authorization = "NONE"
 }
 
+# Respuesta exitosa para el método OPTIONS de "categories_proxy" de la API Gateway
 resource "aws_api_gateway_method_response" "categories_options_200" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.categories_proxy_resource.id
@@ -88,12 +98,13 @@ resource "aws_api_gateway_method_response" "categories_options_200" {
   status_code = "200"
 
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true
-    "method.response.header.Access-Control-Allow-Methods" = true
-    "method.response.header.Access-Control-Allow-Origin"  = true
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true,
   }
 }
 
+# Integración MOCK para el método OPTIONS de "categories_proxy" de la API Gateway
 resource "aws_api_gateway_integration" "categories_options" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.categories_proxy_resource.id
@@ -101,6 +112,7 @@ resource "aws_api_gateway_integration" "categories_options" {
   type        = "MOCK"
 }
 
+# Respuesta exitosa para la integración MOCK del método OPTIONS de "categories_proxy" de la API Gateway
 resource "aws_api_gateway_integration_response" "categories_options_200" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.categories_proxy_resource.id
@@ -108,12 +120,13 @@ resource "aws_api_gateway_integration_response" "categories_options_200" {
   status_code = aws_api_gateway_method_response.categories_options_200.status_code
 
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
-    "method.response.header.Access-Control-Allow-Methods" = "'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT'"
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'",
   }
 }
 
+# Despliegue de la API Gateway
 resource "aws_api_gateway_deployment" "api_deployment" {
   depends_on = [
     aws_api_gateway_integration.get_categories_proxy_integration,
@@ -131,6 +144,7 @@ resource "aws_api_gateway_deployment" "api_deployment" {
   stage_name  = var.api_stage_name
 }
 
+# Etapa de la API Gateway
 resource "aws_api_gateway_stage" "stage" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   deployment_id = aws_api_gateway_deployment.api_deployment.id
@@ -141,15 +155,17 @@ resource "aws_api_gateway_stage" "stage" {
   access_log_settings {
     destination_arn = aws_cloudwatch_log_group.access_logs.arn
     format = "{\"stage\":\"$context.stage\",\"request_id\":\"$context.requestId\",\"api_id\":\"$context.apiId\",\"resource_path\":\"$context.resourcePath\",\"resource_id\":\"$context.resourceId\",\"http_method\":\"$context.httpMethod\",\"source_ip\":\"$context.identity.sourceIp\",\"user-agent\":\"$context.identity.userAgent\",\"account_id\":\"$context.identity.accountId\",\"api_key\":\"$context.identity.apiKey\",\"caller\":\"$context.identity.caller\",\"user\":\"$context.identity.user\",\"user_arn\":\"$context.identity.userArn\"}"
-  }   
+  }
 }
 
+# Grupo de logs en CloudWatch para la API Gateway
 resource "aws_cloudwatch_log_group" "access_logs" {
   name = "/aws/apigateway/${var.api_name}-access-logs"
 
   retention_in_days = 7
 }
 
+# Política de recursos de logs en CloudWatch para la API Gateway
 resource "aws_cloudwatch_log_resource_policy" "log_resource_policy" {
   policy_name = "APIGatewayLogResourcePolicy"
 
@@ -189,6 +205,7 @@ resource "aws_cloudwatch_log_resource_policy" "log_resource_policy" {
 EOF
 }
 
+# Rol IAM para la API Gateway con permisos para CloudWatch Logs
 resource "aws_iam_role" "api_gateway_cloudwatch_role" {
   name = "api-gateway-cloudwatch-role"
   assume_role_policy = <<EOF
@@ -208,6 +225,7 @@ resource "aws_iam_role" "api_gateway_cloudwatch_role" {
 EOF
 }
 
+# Permiso de política IAM para la API Gateway con acceso a CloudWatch Logs
 resource "aws_iam_role_policy_attachment" "api_gateway_cloudwatch_policy_attachment" {
   role       = aws_iam_role.api_gateway_cloudwatch_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs"
