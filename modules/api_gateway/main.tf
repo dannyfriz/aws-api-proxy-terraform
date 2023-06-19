162efbc34804b70184e7676312ed3354c1507c74
dcfriz <dny.friz@gmail.com>
2023-06-19 00:58:56 -0400

    api-ok

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -9,104 +9,146 @@ resource "aws_api_gateway_resource" "categories_resource" {
   path_part   = "categories"
 }
 
-resource "aws_api_gateway_method" "get_categories_method" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.categories_resource.id
-  http_method   = "GET"
-  authorization = "NONE"
+resource "aws_api_gateway_resource" "categories_proxy_resource" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_resource.categories_resource.id
+  path_part   = "{proxy+}"
 }
 
-resource "aws_api_gateway_method" "options_categories_method" {
+resource "aws_api_gateway_method" "get_categories_proxy_method" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.categories_resource.id
+  resource_id   = aws_api_gateway_resource.categories_proxy_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "get_categories_proxy_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.categories_proxy_resource.id
+  http_method             = aws_api_gateway_method.get_categories_proxy_method.http_method
+  integration_http_method = "GET"
+  type                    = "HTTP_PROXY"
+  uri                     = "https://api.mercadolibre.com/categories/{proxy}"
+
+  request_parameters = {
+    "integration.request.path.proxy" = "method.request.path.proxy"
+  }
+
+  passthrough_behavior = "WHEN_NO_MATCH"
+}
+
+resource "aws_api_gateway_method_response" "ok_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.categories_proxy_resource.id
+  http_method = aws_api_gateway_method.get_categories_proxy_method.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "ok_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.categories_proxy_resource.id
+  http_method = aws_api_gateway_method.get_categories_proxy_method.http_method
+  status_code  = aws_api_gateway_method_response.ok_response.status_code
+
+  response_templates = {
+    "application/json" = ""
+  }
+}
+
+resource "aws_api_gateway_method_response" "bad_request_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.categories_proxy_resource.id
+  http_method = aws_api_gateway_method.get_categories_proxy_method.http_method
+  status_code = "400"
+}
+
+resource "aws_api_gateway_integration_response" "bad_request_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.categories_proxy_resource.id
+  http_method = aws_api_gateway_method.get_categories_proxy_method.http_method
+  status_code  = aws_api_gateway_method_response.bad_request_response.status_code
+
+  selection_pattern = "4\\d{2}"
+
+  response_templates = {
+    "application/json" = ""
+  }
+}
+
+resource "aws_api_gateway_method" "categories_options" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.categories_proxy_resource.id
   http_method   = "OPTIONS"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "get_categories_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.categories_resource.id
-  http_method             = aws_api_gateway_method.get_categories_method.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = var.lambda_function_arn
-
-  request_templates = {
-    "application/json" = jsonencode({
-      "statusCode": 200,
-      "body": "I have reached the Lambda function.",
-      "headers": {
-        "Content-Type": "text/plain"
-      }
-    })
-  }
-}
-
-resource "aws_api_gateway_integration" "options_categories_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.categories_resource.id
-  http_method             = aws_api_gateway_method.options_categories_method.http_method
-  integration_http_method = "OPTIONS"
-  type                    = "MOCK"
-}
-
-resource "aws_api_gateway_method_response" "options_categories_response" {
+resource "aws_api_gateway_method_response" "categories_options_200" {
   rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.categories_resource.id
-  http_method = aws_api_gateway_method.options_categories_method.http_method
+  resource_id = aws_api_gateway_resource.categories_proxy_resource.id
+  http_method = aws_api_gateway_method.categories_options.http_method
   status_code = "200"
 
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Methods" = true
-    "method.response.header.Access-Control-Allow-Headers" = true
-    "method.response.header.Access-Control-Allow-Origin"  = true
-  }
-
-  response_models = {
-    "application/json" = "Empty"
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true,
   }
 }
 
-resource "aws_api_gateway_method_response" "get_categories_response" {
+resource "aws_api_gateway_integration" "categories_options" {
   rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.categories_resource.id
-  http_method = aws_api_gateway_method.get_categories_method.http_method
-  status_code = "200"
+  resource_id = aws_api_gateway_resource.categories_proxy_resource.id
+  http_method = aws_api_gateway_method.categories_options.http_method
+  type        = "MOCK"
+}
+
+resource "aws_api_gateway_integration_response" "categories_options_200" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.categories_proxy_resource.id
+  http_method = aws_api_gateway_method.categories_options.http_method
+  status_code = aws_api_gateway_method_response.categories_options_200.status_code
 
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Methods" = true
-    "method.response.header.Access-Control-Allow-Headers" = true
-    "method.response.header.Access-Control-Allow-Origin"  = true
-  }
-
-  response_models = {
-    "application/json" = "Empty"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'",
   }
 }
 
 resource "aws_api_gateway_deployment" "api_deployment" {
   depends_on = [
-    aws_api_gateway_integration.get_categories_integration,
-    aws_api_gateway_integration.options_categories_integration,
-    aws_api_gateway_method.get_categories_method,
-    aws_api_gateway_method.options_categories_method,
-    aws_api_gateway_resource.categories_resource,
+    aws_api_gateway_integration.get_categories_proxy_integration,
+    aws_api_gateway_method.get_categories_proxy_method,
+    aws_api_gateway_resource.categories_proxy_resource,
+    aws_api_gateway_method_response.ok_response,
+    aws_api_gateway_integration_response.ok_integration_response,
+    aws_api_gateway_method.categories_options,
+    aws_api_gateway_method_response.categories_options_200,
+    aws_api_gateway_integration.categories_options,
+    aws_api_gateway_integration_response.categories_options_200
   ]
+
   rest_api_id = aws_api_gateway_rest_api.api.id
   stage_name  = var.api_stage_name
 }
 
-resource "aws_api_gateway_rest_api_policy" "api_policy" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  policy      = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Principal": "*",
-        "Action": "execute-api:Invoke",
-        "Resource": "arn:aws:execute-api:${var.aws_region}:${var.account_id}:${aws_api_gateway_rest_api.api.id}/*"
-      }
-    ]
-  })
+resource "aws_api_gateway_stage" "stage" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  deployment_id = aws_api_gateway_deployment.api_deployment.id
+  stage_name    = "${var.api_stage_name}"
+
+  xray_tracing_enabled = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.access_logs.arn
+    format          = "$context.identity.sourceIp $context.identity.caller $context.identity.user [$context.requestTime] \"$context.httpMethod $context.routeKey $context.protocol\" $context.status $context.responseLength $context.requestId"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "access_logs" {
+  name = "/aws/apigateway/${var.api_name}-access-logs"
 }
--- modules/lambda_function/src/lambda_function.py
+++ modules/lambda_function/src/lambda_function.py
@@ -1,31 +1,65 @@
 import json
+import requests
+import boto3
+from botocore.signers import RequestSigner
+
+def get_api_gateway_url(event):
+    region = event['requestContext']['region']
+    api_id = event['requestContext']['apiId']
+    stage = event['requestContext']['stage']
+
+    url = f"https://{api_id}.execute-api.{region}.amazonaws.com/{stage}"
+    return url
+
+def sign_request(url, region, service):
+    credentials = boto3.Session().get_credentials()
+    signer = RequestSigner(service, region, 'execute-api', 'v4', credentials)
+
+    headers = {}
+    signed_url = signer.generate_presigned_url(
+        method='GET',
+        url=url,
+        headers=headers,
+        region_name=region,
+        expires_in=60
+    )
+
+    return signed_url, headers
 
 def lambda_handler(event, context):
     print("Received event: " + json.dumps(event))
 
     try:
-        if event['path'] == '/categories':
-            print("Lambda function executed for /categories")
-            return {
-                'statusCode': 200,
-                'body': 'I have reached the Lambda function for /categories.',
-                'headers': {
-                    'Content-Type': 'text/plain'
-                }
+        # Extract the category ID from the path
+        category_id = event['pathParameters']['proxy']
+
+        # Create the URL for the API Gateway endpoint
+        api_gateway_url = get_api_gateway_url(event)
+
+        # Sign the request for the API Gateway endpoint
+        signed_url, headers = sign_request(api_gateway_url, 'us-east-1', 'execute-api')
+
+        # Forward the request to the signed URL
+        url = f"{signed_url}/categories/{category_id}"
+        response = requests.get(url, headers=headers)
+
+        # Return the response from api.mercadolibre.com
+        return {
+            'statusCode': response.status_code,
+            'body': response.text,
+            'headers': {
+                'Content-Type': response.headers.get('Content-Type')
             }
-        else:
-            # Your code logic here
-            print("Lambda function executed successfully")
-            return {
-                'statusCode': 200,
-                'body': 'I have reached the Lambda function.',
-                'headers': {
-                    'Content-Type': 'text/plain'
-                }
-            }
+        }
+
     except Exception as e:
         print("Error:", str(e))
-        return {
+
+        error_response = {
             'statusCode': 500,
             'body': 'An error occurred'
         }
+
+        # Log the error response
+        print("Error Response: " + json.dumps(error_response))
+        return error_response
